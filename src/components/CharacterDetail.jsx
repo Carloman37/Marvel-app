d7231464013f0b2bdab7e64ee50b2416f7a653a1
Charles Guyenot <charles.guyenot@etu.univ-poitiers.fr>
2024-12-02 17:36:01 +0100

    version contrôle du lundi 2 decembre

--- src/components/CharacterDetail.jsx
+++ src/components/CharacterDetail.jsx
@@ -1,9 +1,17 @@
+import { format, parseISO, isValid } from 'date-fns';
+
 function CharacterDetail({ character = {} }) {
     // if character is not provided, then render "No character"
     if (!character || Object.keys(character).length === 0) {
         return <div>No character</div>;
     } else {
         // if character is provided, then render the character details
+        const isoDate = character.modified;
+        
+        // Vérifier si la date est valide
+        const dateObj = parseISO(isoDate);
+        const justDate = isValid(dateObj) ? format(dateObj, 'MMM dd, yyyy') : 'Date inconnue';
+
         return (
             <div>
                 <h2>{character.name}</h2>
@@ -12,7 +20,7 @@ function CharacterDetail({ character = {} }) {
                     character.thumbnail && <img src={`${character.thumbnail.path}/standard_large.${character.thumbnail.extension}`} alt={character.name} />
                 }
                 <p>{character.description}</p>
-                <p>{character.modified}</p>
+                <p>{justDate}</p>
             </div>
         );
     }
--- src/components/CharactersList.jsx
+++ src/components/CharactersList.jsx
@@ -1,15 +1,28 @@
 import { Link } from "react-router-dom";
+import { format, parseISO, isValid } from 'date-fns';
 
 export function CharactersList({ characters = [] }) {
   return (
     <ul id="characters">
-      {characters.map((character) => (
-        <li key={character.id}>
-          <Link to={`/characters/${character.id}`}>
-            {character.name}
-          </Link>
-        </li>
-      ))}
+      {characters.map((character) => {
+        // Récupérer la date de chaque personnage
+        const isoDate = character.modified;
+        
+        // Vérifier si la date est valide et la formater
+        const dateObj = parseISO(isoDate);
+        const justDate = isValid(dateObj) ? format(dateObj, 'MMM dd, yyyy') : 'Date inconnue';
+
+        return (
+          <li key={character.id}>
+            <strong>
+              <Link to={`/characters/${character.id}`}>
+                {character.name}
+              </Link>
+            </strong>
+            <small> {justDate}</small> {/* Afficher la date formatée */}
+          </li>
+        );
+      })}
     </ul>
   );
 }
